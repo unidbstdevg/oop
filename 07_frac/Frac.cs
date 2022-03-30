a4a50ab1986a507789dbe4d7fc7713d98378cb9a
Evgeny <sev.20@uni-dubna.ru>
2022-03-30 22:17:47 +0300

    [sem][07_frac] Init

---
+++ 07_frac/Frac.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_frac
+{
+    class Frac
+    {
+        int chisl;
+        int znam;
+
+        public int Chisl
+        {
+            get
+            {
+                return chisl;
+            }
+        }
+        public int Znam
+        {
+            get
+            {
+                return znam;
+            }
+        }
+
+        public Frac()
+        {
+            chisl = znam = 1;
+        }
+        public Frac(int c, int z)
+        {
+            if (z == 0)
+            {
+                Console.WriteLine("[error] Деление на ноль в функции Frac()");
+                Environment.Exit(1);
+            }
+            chisl = c;
+            znam = z;
+
+            // TODO: normalize fraction
+        }
+
+        public override string ToString()
+        {
+            return "(" + chisl + "/" + znam + ")";
+        }
+
+        public static Frac operator *(Frac a, Frac b)
+        {
+            return new Frac(a.chisl * b.chisl, a.znam * b.znam);
+        }
+        public static Frac operator *(int k, Frac b)
+        {
+            return ((new Frac(k, 1)) * b);
+        }
+        public static Frac operator *(Frac a, int k)
+        {
+            return ((new Frac(k, 1)) * a);
+        }
+
+        public double ToDec()
+        {
+            return (1.0 * chisl) / znam;
+        }
+
+        public static bool operator >(Frac a, Frac b)
+        {
+            return a.ToDec() > b.ToDec();
+        }
+        public static bool operator >(Frac a, int k)
+        {
+            return a.ToDec() > k;
+        }
+        public static bool operator >(int k, Frac b)
+        {
+            return k > b.ToDec();
+        }
+
+        public static bool operator >=(Frac a, Frac b)
+        {
+            return a.ToDec() >= b.ToDec();
+        }
+        public static bool operator >=(Frac a, int k)
+        {
+            return a.ToDec() >= k;
+        }
+        public static bool operator >=(int k, Frac b)
+        {
+            return k >= b.ToDec();
+        }
+
+        public static bool operator <(Frac a, Frac b)
+        {
+            return a.ToDec() < b.ToDec();
+        }
+        public static bool operator <(Frac a, int k)
+        {
+            return a.ToDec() < k;
+        }
+        public static bool operator <(int k, Frac b)
+        {
+            return k < b.ToDec();
+        }
+
+        public static bool operator <=(Frac a, Frac b)
+        {
+            return a.ToDec() <= b.ToDec();
+        }
+        public static bool operator <=(Frac a, int k)
+        {
+            return a.ToDec() <= k;
+        }
+        public static bool operator <=(int k, Frac b)
+        {
+            return k <= b.ToDec();
+        }
+
+        public static bool operator ==(Frac a, Frac b)
+        {
+            return a.ToDec() == b.ToDec();
+        }
+        public static bool operator ==(Frac a, int k)
+        {
+            return a.ToDec() == k;
+        }
+        public static bool operator ==(int k, Frac b)
+        {
+            return k == b.ToDec();
+        }
+
+        public static bool operator !=(Frac a, Frac b)
+        {
+            return a.ToDec() != b.ToDec();
+        }
+        public static bool operator !=(Frac a, int k)
+        {
+            return a.ToDec() != k;
+        }
+        public static bool operator !=(int k, Frac b)
+        {
+            return k != b.ToDec();
+        }
+
+        public static Frac operator +(Frac a, Frac b)
+        {
+            int c = a.chisl * b.znam + b.chisl * a.znam;
+            int z = a.znam * b.znam;
+            return new Frac(c, z);
+        }
+        public static Frac operator +(int k, Frac b)
+        {
+            return ((new Frac(k, 1)) + b);
+        }
+        public static Frac operator +(Frac a, int k)
+        {
+            return ((new Frac(k, 1)) + a);
+        }
+
+        public static Frac operator -(Frac a, Frac b)
+        {
+            return a + (b * -1);
+        }
+        public static Frac operator -(int k, Frac b)
+        {
+            return k + (b * -1);
+        }
+        public static Frac operator -(Frac a, int k)
+        {
+            return a + (k * -1);
+        }
+
+        public static Frac operator /(Frac a, Frac b)
+        {
+            return a * (new Frac(b.znam, b.chisl));
+        }
+        public static Frac operator /(int k, Frac b)
+        {
+            return ((new Frac(k, 1)) / b);
+        }
+        public static Frac operator /(Frac a, int k)
+        {
+            return ((a / new Frac(k, 1)));
+        }
+    }
+}
---
+++ 07_frac/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _07_frac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Frac fr;
+
+            Frac f1 = new Frac(1, 2);
+            Frac f2 = new Frac(3, 4);
+            int k = 3;
+
+            fr = f1 * f2;
+            Console.WriteLine("f1 * f2 = " + f1 + " * " + f2 + " = " + fr);
+            fr = f1 * k;
+            Console.WriteLine("f1 * k = " + f1 + " * " + k + " = " + fr);
+            fr = k * f1;
+            Console.WriteLine("k * f1 = " + k + " * " + f1 + " = " + fr);
+
+            fr = f1 + f2;
+            Console.WriteLine("f1 + f2 = " + f1 + " + " + f2 + " = " + fr);
+            fr = f1 + k;
+            Console.WriteLine("f1 + k = " + f1 + " + " + k + " = " + fr);
+            fr = k + f1;
+            Console.WriteLine("k + f1 = " + k + " + " + f1 + " = " + fr);
+
+            fr = f1 - f2;
+            Console.WriteLine("f1 - f2 = " + f1 + " - " + f2 + " = " + fr);
+            fr = f1 - k;
+            Console.WriteLine("f1 - k = " + f1 + " - " + k + " = " + fr);
+            fr = k - f1;
+            Console.WriteLine("k - f1 = " + k + " - " + f1 + " = " + fr);
+
+            fr = f1 / f2;
+            Console.WriteLine("f1 / f2 = " + f1 + " / " + f2 + " = " + fr);
+            fr = f1 / k;
+            Console.WriteLine("f1 / k = " + f1 + " / " + k + " = " + fr);
+            fr = k / f1;
+            Console.WriteLine("k / f1 = " + k + " / " + f1 + " = " + fr);
+
+            Console.WriteLine("f1 > f2? => " + f1 + " > " + f2 + "? => " + (f1 > f2));
+            Console.WriteLine("f1 >= f2? => " + f1 + " >= " + f2 + "? => " + (f1 >= f2));
+            
+            Console.WriteLine("f1 < f2? => " + f1 + " < " + f2 + "? => " + (f1 < f2));
+            Console.WriteLine("f1 <= f2? => " + f1 + " <= " + f2 + "? => " + (f1 <= f2));
+            
+            Console.WriteLine("f1 == f2? => " + f1 + " == " + f2 + "? => " + (f1 == f2));
+            Console.WriteLine("f1 != f2? => " + f1 + " != " + f2 + "? => " + (f1 != f2));
+        }
+    }
+}
