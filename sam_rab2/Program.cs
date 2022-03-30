eab3f29c24b6f0ecf4cc3549193f3b33ea0cba7f
Evgeny <sev.20@uni-dubna.ru>
2022-03-30 22:17:47 +0300

    [sam_rab2] Add demo usage

--- sam_rab2/Program.cs
+++ sam_rab2/Program.cs
@@ -6,7 +6,37 @@ namespace sam_rab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vector a = new Vector(3, 5, 4);
+            Vector b = new Vector(1, 4, 2);
+            double k = 2;
+
+            Console.WriteLine("Vecotr a = " + a);
+            Console.WriteLine("Vecotr b = " + b);
+            Console.WriteLine("double k = " + k);
+            Console.WriteLine();
+
+            Console.WriteLine("a + b = " + (a + b));
+            Console.WriteLine("a * b = " + (a * b));
+            Console.WriteLine("a & b = " + (a & b));
+            Console.WriteLine();
+
+            Console.WriteLine("a * k = " + (a * k));
+            Console.WriteLine("k * a = " + (k * a));
+            Console.WriteLine();
+
+            Console.WriteLine("-a = " + (-a));
+            Console.WriteLine();
+
+            Console.WriteLine("abs(a) = " + a.Abs());
+            Console.WriteLine("abs(b) = " + b.Abs());
+            Console.WriteLine();
+
+            Console.WriteLine("a == b? " + (a == b));
+            Console.WriteLine("a != b? " + (a != b));
+            Console.WriteLine("a > b? " + (a > b));
+            Console.WriteLine("a < b? " + (a < b));
+            Console.WriteLine("a >= b? " + (a >= b));
+            Console.WriteLine("a <= b? " + (a <= b));
         }
     }
 }
