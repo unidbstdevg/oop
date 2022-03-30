2b356d4f4446ac81f35fae52f91f63b95e3e1b5c
Evgeny <sev.20@uni-dubna.ru>
2022-03-30 22:17:47 +0300

    [sem][09_matrix] Init

---
+++ 09_matrix/MyMatrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_matrix
+{
+    class MyMatrix
+    {
+        private double[,] m;
+        private int n, k;
+
+        public double this [int i, int j] {
+            get {
+                if(i < 0 || j < 0 || i >= n || j >= k) {
+                    throw new ApplicationException("Выход за границы матрицы");
+                }
+                return m[i, j];
+            }
+            set {
+                if(i < 0 || j < 0 || i >= n || j >= k) {
+                    throw new ApplicationException("Выход за границы матрицы");
+                }
+                m[i, j] = value;
+            }
+        }
+
+        public MyMatrix() {
+            n = k = 1;
+            m = new double[n, k];
+        }
+
+        public MyMatrix(int nn, int kk) {
+            if(nn == 0 || kk == 0) {
+                throw new ApplicationException("Размерность матрицы не может быть равна 0");
+            }
+
+            n = nn;
+            k = kk;
+            m = new double[n, k];
+        }
+
+        public int N {
+            get {
+                return n;
+            }
+        }
+        public int K {
+            get {
+                return k;
+            }
+        }
+
+        public void Print() {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < k; j++) {
+                    Console.Write(m[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 09_matrix/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09_matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MyMatrix a = new MyMatrix(2, 3);
+                a[0, 0] = 5;
+                a[1, 1] = 9;
+                a.Print();
+                a[10, 10] = 3;
+                Console.WriteLine("Lmao u'll never see this tExt. shit!");
+            } catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
