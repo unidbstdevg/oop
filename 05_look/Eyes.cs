28507d71d97393a25cd11b0afca1b7828dc213b2
Evgeny <sev.20@uni-dubna.ru>
2022-03-30 22:17:47 +0300

    [dz] Look

---
+++ 05_look/Eyes.cs
@@ -0,0 +1,84 @@
+using System.CodeDom;
+using _03_ellipse;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+
+namespace _05_look
+{
+    class Eye {
+        MyEllipse ball;
+        MyEllipse pupil;
+
+        // pupil
+        int r;
+        // ball
+        int R;
+
+        // coords of center
+        int cx;
+        int cy;
+
+        public Eye(int x, int y, int _R, Color clr) {
+            R = _R;
+
+            ball = new MyEllipse(x, y, R, R, Color.Black, Color.White);
+
+            r = R / 3;
+            cx = x + r;
+            cy = y + r;
+            pupil = new MyEllipse(cx, cy, r, r, Color.White, clr);
+        }
+
+        public void Draw(Graphics gr)
+        {
+            ball.Draw(gr);
+            pupil.Draw(gr);
+        }
+
+        public void LookAt(int x, int y) {
+            int dy = y - pupil.Y;
+            int dx = x - pupil.X;
+            double gipotenyza = Math.Sqrt(dy * dy + dx * dx);
+            double ky =  dy / gipotenyza;
+            double kx =  dx / gipotenyza;
+
+            double ddy = ky * R;
+            double ddx = kx * R;
+            pupil.X = cx + (int)(ddx / 2);
+            pupil.Y = cy + (int)(ddy / 2);
+        }
+
+    }
+    public class Eyes
+    {
+        private int x;
+        private int y;
+        private int r;
+
+        Eye leye;
+        Eye reye;
+        private int gap;
+
+        public Eyes(int X, int Y, int R = 10, int Gap = 0) {
+            x = X;
+            y = Y;
+            r = R;
+
+            leye = new Eye(x, y, r, Color.Red);
+            reye = new Eye(x + r, y, r, Color.Blue);
+        }
+
+        public void Draw(Graphics gr)
+        {
+            leye.Draw(gr);
+            reye.Draw(gr);
+        }
+
+        public void LookAt(int x, int y) {
+            leye.LookAt(x, y);
+            reye.LookAt(x, y);
+        }
+    }
+}
---
+++ 05_look/Form1.cs
@@ -0,0 +1,52 @@
+﻿using _03_ellipse;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_look
+{
+    public partial class Form1 : Form
+    {
+        List<Eyes> eyes;
+        Graphics gr;
+        public Form1()
+        {
+            InitializeComponent();
+
+            gr = panel1.CreateGraphics();
+            eyes = new List<Eyes>();;
+
+            eyes.Add(new Eyes(250, 250, 50));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            int r = rnd.Next(35, Math.Min(panel1.Width, panel1.Height) / 3);
+            Eyes es = new Eyes(rnd.Next(panel1.Width - 2 * r), rnd.Next(panel1.Height - r), r);
+            eyes.Add(es);
+
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(Eyes es in eyes)
+                es.Draw(gr);
+        }
+
+        private void panel1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e) {
+            foreach(Eyes es in eyes)
+                es.LookAt(e.X, e.Y);
+
+            panel1.Refresh();
+        }
+
+    }
+}
